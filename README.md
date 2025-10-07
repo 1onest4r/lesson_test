455b9f096b6765549d5fbff5df0db39e022daa85
1onest4r <2024054@miu.edu.mn>
2025-10-07 11:27:42 +0800

    testing header in README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # lesson_test
-lul
+Hash_tables practice 
 
 This is me Eric! I'm having trouble with ubuntu since the 
 class started.
+
+# H1
+## H2
+### H3
--- test.dart
+++ test.dart
@@ -1,13 +1,1 @@
-void main() {
-  class Hello {
-    late final greet; 
-    Hello(this.greet);
-    @override 
-    String toString() {
-      final result = "hi";
-      return result.toString();
-    }
-  }
-  final hi = Hello("hi");
-  print(result);
-}
+
