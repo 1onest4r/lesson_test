b1693f5b149418e7044a35201c6e5a5fd7440841
1onest4r <2024054@miu.edu.mn>
2025-10-07 12:05:25 +0800

    added test option in test hashtables_test.dart for linear_seach

--- test/hashtables_test.dart
+++ test/hashtables_test.dart
@@ -1,8 +1,13 @@
 import 'package:hashtables/hashtables.dart';
+import 'package:hashtables/linear_search.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
+  });
+
+  test('linear search', () {
+    expect(linearSearch([1, 2, 3, 4, 5, 6, 7], 3), 2);
   });
 }
