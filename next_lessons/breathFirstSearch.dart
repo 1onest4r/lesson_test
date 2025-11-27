3631856953062faa24e88721f9f73e6481c81964
1onest4r <2024054@miu.edu.mn>
2025-11-27 12:53:06 +0800

    added implementation of breathFirstSearch.dart

---
+++ next_lessons/breathFirstSearch.dart
@@ -0,0 +1,203 @@
+class Edge<T> {
+  final T destination;
+  final int weight;
+  Edge(this.destination, this.weight);
+
+  @override
+  String toString() {
+    return '($destination, W:$weight)';
+  }
+}
+
+class Graph<T> {
+  final Map<T, List<Edge<T>>> _adjacencyList = {};
+  final bool directed;
+  Graph({this.directed = false});
+
+  void addVertex(T vertex) {
+    if (_adjacencyList.containsKey(vertex)) return;
+    _adjacencyList[vertex] = [];
+  }
+
+  void addEdge(T source, T destination, {int weight = 1}) {
+    // Create source and destination if they don't exist yet
+    addVertex(source);
+    addVertex(destination);
+
+    // add an edge between source and destination
+    _adjacencyList[source]!.add(Edge(destination, weight));
+
+    // if this is an undirected graph, add edge from destination to source
+    if (!directed) {
+      _adjacencyList[destination]!.add(Edge(source, weight));
+    }
+  }
+
+  void breathFirstSearch(T startVertex) {
+    // make the queue
+    final queue = LinkedListQueue<T>();
+
+    // keep track of all the visited vertices
+    final visited = <T>{};
+
+    // initialize queue
+    queue.enqueue(startVertex);
+    visited.add(startVertex);
+
+    // keep going until the queue is empty
+    while (!queue.isEmpty) {
+      // dequeue the next item
+      final current = queue.dequeue();
+      if (current == null) break;
+
+      _processVertex(current);
+
+      // visit all the neighbors
+      for (final neighbor in _adjacencyList[current]!) {
+        if (!visited.contains(neighbor.destination)) {
+          queue.enqueue(neighbor.destination);
+        }
+      }
+    }
+  }
+
+  void _processVertex(T vertex) {
+    print(vertex);
+  }
+
+  @override
+  String toString() {
+    final buffer = StringBuffer();
+    _adjacencyList.forEach((vertex, edges) {
+      String connections = edges
+          .map((e) => '${e.destination}(${e.weight})')
+          .join(', ');
+      buffer.writeln('$vertex: $connections');
+    });
+    return buffer.toString();
+  }
+}
+
+class Node<T> {
+  Node(this.value, [this.next]);
+
+  T value;
+  Node<T>? next;
+
+  @override
+  String toString() {
+    final result = StringBuffer();
+    result.write(value);
+    var nextNode = next;
+    while (nextNode != null) {
+      result.write(' -> ');
+      result.write(nextNode.value);
+      nextNode = nextNode.next;
+    }
+    return result.toString();
+  }
+}
+
+class LinkedList<T> {
+  Node<T>? head;
+  Node<T>? tail;
+
+  void addLast(T value) {
+    final newNode = Node(value);
+    if (isEmpty) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail?.next = newNode;
+      tail = newNode;
+    }
+  }
+
+  void addFirst(T value) {
+    final newNode = Node(value);
+    if (isEmpty) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+    }
+  }
+
+  T? removeFirst() {
+    if (isEmpty) {
+      throw Exception('The list is empty.');
+    }
+
+    final returnValue = head;
+    head = head?.next;
+    return returnValue?.value;
+  }
+
+  void removeLast() {
+    if (isEmpty) {
+      throw Exception('The list is empty.');
+    }
+
+    if (head == tail) {
+      // one item in the list
+      head = null;
+      tail = null;
+    } else {
+      // many items in the list
+      var current = head;
+      while (current?.next != tail) {
+        current = current?.next;
+      }
+      current?.next = null;
+      tail = current;
+    }
+  }
+
+  bool get isEmpty => head == null;
+
+  @override
+  String toString() {
+    if (isEmpty) {
+      return 'Empty';
+    }
+    return head.toString();
+  }
+}
+
+class LinkedListQueue<T> {
+  LinkedList<T> _data = LinkedList<T>();
+
+  bool get isEmpty => _data.isEmpty;
+
+  void enqueue(T value) {
+    _data.addLast(value);
+  }
+
+  T? dequeue() {
+    return _data.removeFirst();
+  }
+
+  @override
+  String toString() {
+    return _data.toString();
+  }
+}
+
+void main() {
+  final myGraph = Graph();
+  myGraph.addEdge("A", "B");
+  myGraph.addEdge("A", "E");
+  myGraph.addEdge("A", "C");
+  myGraph.addEdge("B", "C");
+  myGraph.addEdge("B", "E");
+  myGraph.addEdge("B", "D");
+  myGraph.addEdge("C", "E");
+  myGraph.addEdge("C", "G");
+  myGraph.addEdge("D", "G");
+  myGraph.addEdge("E", "F");
+  myGraph.addEdge("F", "H");
+  myGraph.addEdge("G", "H");
+
+  print(myGraph);
+}
--- next_lessons/review_1min/test.dart
+++ next_lessons/review_1min/test.dart
@@ -1,16 +1,1 @@
 
-// A. Print the multiplication table up to 12x12. (1x1=1, 1x2=2, ...)
-// B. Create a set with five numbers. Check if a number exists in the set.
-
-void main() {
-  // for (int i = 1; i < 13; i++) {
-  //   String row = "";
-  //   for (int j = 1; j < 13; j++) {
-  //     row += "${i*j} ";
-  //   }
-  //   print(row);
-  // }
-
-  final myList = [3, 2, 1];
-  print(myList.contains(3));
-}
